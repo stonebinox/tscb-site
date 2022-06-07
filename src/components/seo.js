5338598c9f7874d3ba102c856fdf6e4d0b4ef7f5
Anoop Santhanam <anoop.santhanam@gmail.com>
2022-06-07 23:30:49 +0530

    Setting up basic components

---
+++ src/components/seo.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+
+function Seo({ description, lang, meta, title }) {
+  const metaDescription = description || "";
+  const defaultTitle = title || "";
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  );
+}
+
+export default Seo;
