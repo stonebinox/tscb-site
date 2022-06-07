6c66dde84c113014be2f1a71bd9d907c4f6cb430
Anoop Santhanam <anoop.santhanam@gmail.com>
2022-06-07 22:03:23 +0530

    changed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: `tscb`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "6k2OTPgYLY4i14X6IXceSNcdMgqPO2fQAufqiEjfZfE",
-      "spaceId": "at3ui5p9qbcn"
-    }
-  }, "gatsby-plugin-react-helmet"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "6k2OTPgYLY4i14X6IXceSNcdMgqPO2fQAufqiEjfZfE",
+        spaceId: "at3ui5p9qbcn",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+  ],
 };
