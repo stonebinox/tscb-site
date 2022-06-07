db50b03ebc59e32aff107a83708f23d9ae617524
Anoop Santhanam <anoop.santhanam@gmail.com>
2022-06-07 22:21:10 +0530

    changed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,29 @@ module.exports = {
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/emarald.png`,
+      },
+    },
   ],
 };
